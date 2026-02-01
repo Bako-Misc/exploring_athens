d32cfa0e57f777e117b076cea842b39a86f01133
Nikos Bakogiannis <nbakojohn13@gmail.com>
2026-02-01 13:29:00 +0100

    adjust for github actiobs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/exploring_athens/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        caffee: resolve(__dirname, "caffee.html"),
+        lunch: resolve(__dirname, "lunch.html"),
+        visit: resolve(__dirname, "visit.html"),
+      },
+    },
+  },
 });
